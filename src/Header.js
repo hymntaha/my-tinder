54d08b22f34ed915ccd37a652fd4deb99c7f58cc
hymntaha <tahauygun@gmail.com>
2020-08-13 16:50:48 -0700

    development starts

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import "./Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 const MyComponent = () => {
   return (
-    <div>
+    // BEM
+    <div className="header">
+      <PersonIcon/>
       <h1>I am header</h1>
+      <ForumIcon/>
     </div>
   );
 };
