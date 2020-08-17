d4df2d4ca932bc49294bb650050a019f5347196f
hymntaha <tahauygun@gmail.com>
2020-08-17 12:26:35 -0700

    adding tinder card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>My tinder clone</h1>
+
+      <Router>
+        <Switch>
+          <Router path="/chat">
+
+          </Router>
+          <Router path="/">
+
+          </Router>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
